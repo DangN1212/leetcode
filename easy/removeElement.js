7f1ded1cccedc9720f88deb4d0d0739c02a42886
Dan Nguyen <haidang121292@gmail.com>
2022-10-13 23:25:49 +0700

    Update removeElement function

--- easy/removeElement.js
+++ easy/removeElement.js
@@ -4,7 +4,33 @@
  * @return {number}
  */
  var removeElement = function(nums = [], val) {
-    
+     //Solution 1
+    // let indexUpdate = 0;
+    // for (let indexArr = 0; indexArr < nums.length; indexArr++) {
+    //     if(nums[indexArr] !== val){
+    //         nums[indexUpdate] = nums[indexArr];
+    //         // let temp = nums[indexArr];
+    //         // nums[indexArr] = nums[indexUpdate]
+    //         // nums[indexUpdate] = temp;
+    //         indexUpdate++;
+    //     }
+        
+    // }
+
+    // return indexUpdate;
+
+    //solution 2
+
+    for (let index = 0; index < nums.length; index++) {
+        if(nums[index]=== val){
+            nums.splice(index,1);
+            index--
+        }
+
+        
+    }
+
+    return nums.length
 };
 
-module.exports = removeElement
+module.exports = removeElement;
