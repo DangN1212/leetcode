535a9deb9b48c65fe86d8bb40dde64708e334d4b
Dan Nguyen <haidang121292@gmail.com>
2023-06-11 00:07:49 +0700

    Duplicate zeros

---
+++ medium/generateParenthese.js
@@ -0,0 +1,10 @@
+/**
+ * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ * Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+ *  @param {number} n
+ *  @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    
+};
